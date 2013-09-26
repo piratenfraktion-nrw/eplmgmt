e2264381001ab68f4650759ab4891c9e9a38bf29
vileda <vileda@piratenfraktion-nrw.de>
2013-09-26 21:53:30 +0200

    fixing bugs

--- app/helpers/pads_helper.rb
+++ app/helpers/pads_helper.rb
@@ -7,11 +7,15 @@ module PadsHelper
   end
 
   def fetch_ep_last_edited(pad)
-    _pad = Pad.find(pad.id)
-    last_edit = DateTime.strptime(pad.ep_pad.last_edited.to_s, '%Q')
-    if last_edit.to_time.to_i > _pad.edited_at.to_time.to_i
-      _pad.edited_at = last_edit
-      _pad.save
+    begin
+      _pad = Pad.find(pad.id)
+      last_edit = DateTime.strptime(pad.ep_pad.last_edited.to_s, '%Q')
+      if last_edit.to_time.to_i > _pad.edited_at.to_time.to_i
+        _pad.edited_at = DateTime.now
+        _pad.save
+      end
+    rescue
+      logger.error "HIT THE FUCKING BUG"
     end
   end
 end
