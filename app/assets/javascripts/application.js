7f41791f21fff8a06669c05bc10728472011120d
vileda <vileda@piratenfraktion-nrw.de>
2013-10-08 15:28:30 +0200

    fixed loader

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,37 +1,45 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require bootstrap-modal
-//= require bootstrap-modalmanager
-//= require turbolinks
-//= require select2
-//= require_tree .
+/*
+ This is a manifest file that'll be compiled into application.js, which will include all the files
+ listed below.
+
+ Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+ or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+
+ It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+ compiled file.
+
+ Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+ about supported directives.
+
+ *= require jquery
+ *= require jquery_ujs
+ *= require bootstrap
+ *= require bootstrap-modal
+ *= require bootstrap-modalmanager
+ *= require turbolinks
+ *= require select2
+ *= require_tree .
+ */
 
 var loaderTimeout;
 function ready() {
-  $('select').select2();
+    $('select').select2();
 }
 
-$(document).on('page:fetch', function() {
-  loaderTimeout = setTimeout(function() {
-    $('.content').modalmanager('loading');
-  }, 300);
+$(window).bind('popstate', function () {
+    $('.modal-backdrop').remove();
+    $('.modal-scrollable').remove();
 });
 
-$(document).on('page:change', function() {
-  clearTimeout(loaderTimeout);
+$(document).on('page:fetch', function () {
+    window.loaderTimeout = setTimeout(function () {
+        $('.content').modalmanager('loading');
+    }, 300);
+});
+
+$(document).on('page:change', function () {
+    clearTimeout(window.loaderTimeout);
+    window.loaderTimeout = null;
 });
 
 $(document).ready(ready);
