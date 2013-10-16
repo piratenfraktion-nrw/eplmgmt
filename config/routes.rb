d45df67b5ee7b1ce0166eb69f7408fe96bbc0dc8
vileda <vileda@piratenfraktion-nrw.de>
2013-10-16 15:53:49 +0200

    rails doesn't allow Regexp anchors in routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Eplmgmt::Application.routes.draw do
     resources :pads
   end
 
-  get '/p/:pad', to: 'pads#show', as: 'named_pad', only: [:show], format: false, pad: /\A[\.[:alnum:][:space:],%_-]+\z/
-  get '/p/:group/:pad', to: 'pads#show', as: 'named_group_pad', only: [:show], format: false, group: /\A[\.[:alnum:][:space:],%_-]+\z/, pad: /\A[\.[:alnum:][:space:],%_-]+\z/
+  get '/p/:pad', to: 'pads#show', as: 'named_pad', only: [:show], format: false, pad: /[\.[:alnum:][:space:],%_-]+/
+  get '/p/:group/:pad', to: 'pads#show', as: 'named_group_pad', only: [:show], format: false, group: /[\.[:alnum:][:space:],%_-]+/, pad: /[\.[:alnum:][:space:],%_-]+/
   get '/p', to: 'home#pads', as: 'named_pads'
 
   root :to => "home#index"
