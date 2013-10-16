9344a3facdcf10eae1f26d664087c76d5319341f
vileda <vileda@piratenfraktion-nrw.de>
2013-10-16 16:07:23 +0200

    remormatted routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,25 @@ Eplmgmt::Application.routes.draw do
     resources :pads
   end
 
-  get '/p/:pad', to: 'pads#show', as: 'named_pad', only: [:show], format: false, pad: /[\.[:alnum:][:space:],%_-]+/
-  get '/p/:group/:pad', to: 'pads#show', as: 'named_group_pad', only: [:show], format: false, group: /[\.[:alnum:][:space:],%_-]+/, pad: /[\.[:alnum:][:space:],%_-]+/
+  get '/p/:pad',
+      to: 'pads#show',
+      as: 'named_pad',
+      only: [:show],
+      format: false,
+      constraints: {
+          pad: /[\.[:alnum:][:space:],%_-]+/
+      }
+
+  get '/p/:group/:pad',
+      to: 'pads#show',
+      as: 'named_group_pad',
+      only: [:show],
+      format: false,
+      constraints: {
+          group: /[\.[:alnum:][:space:],%_-]+/,
+          pad: /[\.[:alnum:][:space:],%_-]+/
+      }
+
   get '/p', to: 'home#pads', as: 'named_pads'
 
   root :to => "home#index"
