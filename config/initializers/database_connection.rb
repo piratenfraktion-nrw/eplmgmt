242dd023999e1827106c5f5fd905bab482f6b1e6
vileda <vileda@piratenfraktion-nrw.de>
2014-02-07 10:29:56 +0100

    updated gems, increased db connections

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -4,7 +4,7 @@ Rails.application.config.after_initialize do
   ActiveSupport.on_load(:active_record) do
     config = Rails.application.config.database_configuration[Rails.env]
     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-    config['pool']              = ENV['DB_POOL']      || 2
+    config['pool']              = ENV['DB_POOL']      || 25
     ActiveRecord::Base.establish_connection(config)
   end
 end
