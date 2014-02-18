76ecea17c81e10711244fe47a136a37716adae74
vileda <vileda@piratenfraktion-nrw.de>
2014-02-18 14:21:26 +0100

    use unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,27 @@
+# config/unicorn.rb
+worker_processes Integer(3)
+timeout 6
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  if defined?(ActiveRecord::Base)
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = 10 # seconds
+    config['pool']            = 5
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
